e44aa223f26c015c2798405d087348b04703981b
kaleidoscopeworld <userabcc+github@gmail.com>
2019-06-19 10:02:57 +0800

    changed application.properties

--- community/src/main/java/com/example/community/controller/AuthorizeController.java
+++ community/src/main/java/com/example/community/controller/AuthorizeController.java
@@ -1,13 +1,45 @@
 package com.example.community.controller;
 
+import com.example.community.dto.AccessTokenDTO;
+import com.example.community.dto.GithubUser;
+import com.example.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+
+    @Value("${github.client.id}")
+    private String client_id;
+
+    @Value("${github.client.secret}")
+    private String client_secret;
+
+    @Value("${github.redirect.uri}")
+    private String redirect_uri;
+
+
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code) {
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id(client_id);
+        accessTokenDTO.setClient_secret(client_secret);
+        accessTokenDTO.setRedirect_uri(redirect_uri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getId());
+        System.out.println(user.getBio());
         return "index";
     }
 }
