7c697d61a8c32829d6207646a4db920bb3c8546a
kaleidoscopeworld <userabcc+github@gmail.com>
2019-06-20 21:54:29 +0800

    qaq

--- community/src/main/java/com/example/community/controller/IndexController.java
+++ community/src/main/java/com/example/community/controller/IndexController.java
@@ -22,7 +22,7 @@ public class IndexController {
         Cookie[] cookies = request.getCookies();
 
         for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")) {
+            if (cookie.equals("token")) {
                 String token = cookie.getValue();
                 User user = userMapper.findByToken(token);
 
